60fdd7dc3594d0a76329177209444033f34adc91
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-04-29 23:43:11 +0300

    Update Naive_Bayes.py

--- src/Naive_Bayes.py
+++ src/Naive_Bayes.py
@@ -14,6 +14,8 @@ import seaborn as sns
 from scipy.stats import norm
 from statistics import stdev, mean
 
+from sklearn.metrics import f1_score
+
 
 
 NUM_OF_GHOULS = 0
@@ -21,11 +23,45 @@ NUM_OF_GOBLINS = 0
 NUM_OF_GHOSTS = 0
 
 
+_ghoul = []
+_goblin = []
+_ghost = []
+
+# omega(j): one of the following types: Ghoul, Goblin, Ghost.
+
+def plot_data(df):
+    pass
+
+
 
 def get_type_id_pair(df):
+    predicted = []
     # use this for metrics calculation.
     id_type_unordered_pair = [(monster_id, monster_type) for monster_id in df['id'] for monster_type in df.loc[(df['id'] == monster_id)]["type"]]
-    return id_type_unordered_pair
+
+    _ghoul_max = max(_ghoul)
+    _goblin_max = max(_goblin)
+    _ghost_max = max(_ghost)
+
+    max_id = max(_ghoul_max, _goblin_max, _ghost_max)
+
+    for i in range(0, max_id + 1):
+        if i in _ghoul:
+            predicted.append(0)
+        if i in _goblin:
+            predicted.append(1)
+        if i in _ghost:
+            predicted.append(2)
+
+    return predicted
+
+
+
+
+
+
+
+    # return id_type_unordered_pair
 
 
 
@@ -70,6 +106,14 @@ def store_train_data(csv_path: str):
 
     return df
 
+def multinomial_pdf(x_value, type_array):
+    pass
+
+
+
+
+
+
 
 def gauss_pdf(x_value, type_array, feature_id):
     # calcuates the likelihood probability = p(x|omega(type)), given that x follows gaussian distribution
@@ -88,6 +132,7 @@ def gauss_pdf(x_value, type_array, feature_id):
     gauss_exp_denominator = 2 * (sample_variance)
     term2 = np.exp(gauss_exp_numerator / gauss_exp_denominator)
 
+
     return (term1 * term2)
 
 def main(*args):
@@ -98,25 +143,68 @@ def main(*args):
     samples = [np.array(df.loc[df['type'] == idx]) for idx, el in enumerate(classes)]
 
     ghoul_array = samples[0]
-    goblins_array = samples[1]
+    goblin_array = samples[1]
     ghost_array = samples[2]
 
-    print_test(df, ghoul_array, goblins_array, ghost_array)
+    naive_Bayes(df, ghoul_array, goblin_array, ghost_array)
+
+    predicted = get_type_id_pair(df)
+
+    true = [df['type'][i] for i in range((len(df)))]
+
+    f1_score(true, predicted, average='weighted')
 
 
 
 
-def print_test(df, ghoul_array, goblins_array, ghost_array):
+def get_max_index(list_score):
+    max_value = max(list_score)
+    max_index = list_score.index(max_value)
+
+    return max_index
+
+
+def naive_Bayes(df, ghoul_array, goblin_array, ghost_array):
+    global _ghoul, _goblin, _ghost
+
+    _ghoul = []
+    _goblin = []
+    _ghost = []
+
     for i in range(len(df)):
-        print("Element no. {}:".format(i))
-        for j in range(1, 5):
-            print(gauss_pdf(np.array(df)[i][j], ghoul_array, j))
-            print(gauss_pdf(np.array(df)[i][j], goblins_array, j))
-            print(gauss_pdf(np.array(df)[i][j], ghost_array, j))
+        ghost_probability = []
+        goblin_probability = []
+        ghoul_probability = []
 
-            if j != 4:
-                print('\n' * 1)
-        print("-----------------------------------------------------------")
+        for j in range(1, 5):
+            ghoul_probability.append(gauss_pdf(np.array(df)[i][j], ghoul_array, j))
+            goblin_probability.append(gauss_pdf(np.array(df)[i][j], goblin_array, j))
+            ghost_probability.append(gauss_pdf(np.array(df)[i][j], ghost_array, j))
+
+            #print(gauss_pdf(np.array(df)[i][j], ghoul_array, j))
+            #print(gauss_pdf(np.array(df)[i][j], goblins_array, j))
+            #print(gauss_pdf(np.array(df)[i][j], ghost_array, j))
+
+        ghoul_ = np.array(ghoul_probability)
+        goblin_ = np.array(goblin_probability)
+        ghost_ = np.array(ghost_probability)
+
+        ghoul_score = np.prod(ghoul_)
+        goblin_score = np.prod(goblin_)
+        ghost_score = np.prod(ghost_)
+
+        list_score = [ghoul_score, goblin_score, ghost_score]
+
+        idx = get_max_index(list_score)
+
+        if idx == 0:
+            _ghoul.append(df['id'][i])
+        elif idx == 1:
+            _goblin.append(df['id'][i])
+        elif idx == 2:
+            _ghost.append(df['id'][i])
+
+
 
 if __name__ == "__main__":
 	main()
