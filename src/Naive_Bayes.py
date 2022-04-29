c083ddae29ee89da7349844c0de5a2ff38102504
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-04-30 00:24:33 +0300

    Update Naive_Bayes.py

--- src/Naive_Bayes.py
+++ src/Naive_Bayes.py
@@ -37,7 +37,7 @@ def plot_data(df):
 def get_type_id_pair(df):
     predicted = []
     # use this for metrics calculation.
-    id_type_unordered_pair = [(monster_id, monster_type) for monster_id in df['id'] for monster_type in df.loc[(df['id'] == monster_id)]["type"]]
+    # id_type_unordered_pair = [(monster_id, monster_type) for monster_id in df['id'] for monster_type in df.loc[(df['id'] == monster_id)]["type"]]
 
     _ghoul_max = max(_ghoul)
     _goblin_max = max(_goblin)
@@ -74,7 +74,7 @@ def calculate_type_appearances(df):
 
 
 
-def store_train_data(csv_path: str):
+def store_data(csv_path: str, type: str):
     df = pd.read_csv(csv_path)
     cols = df.columns
     cols = [col for col in cols]
@@ -96,15 +96,20 @@ def store_train_data(csv_path: str):
             df.at[i, 'color'] = 6
 
 
-
-        if df.at[i, 'type'] == "Ghoul":
-            df.at[i, 'type'] = 0
-        elif df.at[i, 'type'] == "Goblin":
-            df.at[i, 'type'] = 1
-        elif df.at[i, 'type'] == "Ghost":
-            df.at[i, 'type'] = 2
+        if type == "train":
+            if df.at[i, 'type'] == "Ghoul":
+                df.at[i, 'type'] = 0
+            elif df.at[i, 'type'] == "Goblin":
+                df.at[i, 'type'] = 1
+            elif df.at[i, 'type'] == "Ghost":
+                df.at[i, 'type'] = 2
 
     return df
+
+
+
+
+
 
 def multinomial_pdf(x_value, type_array):
     pass
@@ -115,17 +120,8 @@ def multinomial_pdf(x_value, type_array):
 
 
 
-def gauss_pdf(x_value, type_array, feature_id):
+def gauss_pdf(x_value, sample_mean, sample_variance):
     # calcuates the likelihood probability = p(x|omega(type)), given that x follows gaussian distribution
-
-    if feature_id not in range(1, 5):
-        sys.exit("Feature id must in range [1, 4].")
-
-    sample_mean = np.mean(type_array, 0)[feature_id]
-    sample_variance = np.var(type_array, 0)[feature_id]
-
-    # term11 = -(np.sum(np.log(np.sqrt(sample_variance))))
-    # term22 = -(np.sum((x_value - sample_mean) ** 2 / (2 * sample_variance)))
 
     term1 = 1 / (np.sqrt(2 * np.pi * (sample_variance)))
     gauss_exp_numerator = - np.power((x_value - sample_mean), 2)
@@ -138,28 +134,43 @@ def gauss_pdf(x_value, type_array, feature_id):
 
 
 
-def fit():
-    pass
+def fit(df):
 
-
-
-def main(*args):
-    df = store_train_data("train.csv")
     calculate_type_appearances(df)
     classes = np.unique(df.type)
 
     samples = [np.array(df.loc[df['type'] == idx]) for idx, el in enumerate(classes)]
 
-
     ghoul_array = samples[0]
     goblin_array = samples[1]
     ghost_array = samples[2]
 
-    naive_Bayes(df, ghoul_array, goblin_array, ghost_array)
 
-    predicted = get_type_id_pair(df)
+    mean_values_ghoul = [np.mean(ghoul_array, 0)[i] for i in range(1, 5)]
+    variances_ghoul = [np.var(ghoul_array, 0)[i] for i in range(1, 5)]
 
-    true = [df['type'][i] for i in range((len(df)))]
+    mean_values_goblin = [np.mean(goblin_array, 0)[i] for i in range(1, 5)]
+    variances_goblin = [np.var(goblin_array, 0)[i] for i in range(1, 5)]
+
+    mean_values_ghost = [np.mean(ghost_array, 0)[i] for i in range(1, 5)]
+    variances_ghost = [np.var(ghost_array, 0)[i] for i in range(1, 5)]
+
+    #print(mean_values_ghoul)
+
+    return (mean_values_ghoul, variances_ghoul, mean_values_goblin, variances_goblin, mean_values_ghost, variances_ghost)
+
+def main(*args):
+    train_df = store_data("train.csv", "train")
+    naive_bayes_args = fit(train_df)
+
+    test_df = store_data("test.csv", "test")
+
+    naive_Bayes(train_df, naive_bayes_args[0], naive_bayes_args[1], naive_bayes_args[2], naive_bayes_args[3]
+                , naive_bayes_args[4], naive_bayes_args[5])
+
+    predicted = get_type_id_pair(train_df)
+
+    true = [train_df['type'][i] for i in range((len(train_df)))]
 
     print(f1_score(true, predicted, average='weighted'))
 
@@ -167,7 +178,7 @@ def main(*args):
 
 
 
-def naive_Bayes(df, ghoul_array, goblin_array, ghost_array):
+def naive_Bayes(df, mean_values_ghoul, variances_ghoul, mean_values_goblin, variances_goblin, mean_values_ghost, variances_ghost):
     global _ghoul, _goblin, _ghost
 
     _ghoul = []
@@ -180,9 +191,9 @@ def naive_Bayes(df, ghoul_array, goblin_array, ghost_array):
         ghoul_probability = []
 
         for j in range(1, 5):
-            ghoul_probability.append(gauss_pdf(np.array(df)[i][j], ghoul_array, j))
-            goblin_probability.append(gauss_pdf(np.array(df)[i][j], goblin_array, j))
-            ghost_probability.append(gauss_pdf(np.array(df)[i][j], ghost_array, j))
+            ghoul_probability.append(gauss_pdf(np.array(df)[i][j], mean_values_ghoul[j - 1], variances_ghoul[j - 1]))
+            goblin_probability.append(gauss_pdf(np.array(df)[i][j], mean_values_goblin[j - 1], variances_goblin[j - 1]))
+            ghost_probability.append(gauss_pdf(np.array(df)[i][j], mean_values_ghost[j - 1], variances_ghost[j - 1]))
 
             #print(gauss_pdf(np.array(df)[i][j], ghoul_array, j))
             #print(gauss_pdf(np.array(df)[i][j], goblins_array, j))
@@ -197,7 +208,6 @@ def naive_Bayes(df, ghoul_array, goblin_array, ghost_array):
         ghost_score = np.prod(ghost_)
 
         list_score = [ghoul_score, goblin_score, ghost_score]
-
         idx = np.argmax(list_score)
 
         if idx == 0:
